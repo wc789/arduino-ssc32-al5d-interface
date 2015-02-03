c9ccff6dd838b636c0a73482ed9cffd87d85848c
Petri Nissilä <petri.nissila@outlook.com>
2015-02-03 16:08:37 +0200

    Sketched outline for program
    Added setup() and loop() declarations. Added values for servo positions
    and limits. Declared some probably useful functions.

---
+++ arduino-ssc32-al5d-interface.ino
@@ -0,0 +1,45 @@
+// Variables for servo values and limits
+int servo1_value;
+const int servo1_upper_limit;
+const int servo1_lower_limit;
+
+int Servo2_value;
+const int servo2_upper_limit;
+const int servo2_lower_limit;
+
+int Servo3_value;
+const int servo3_upper_limit;
+const int servo3_lower_limit;
+
+int Servo4_value;
+const int servo4_upper_limit;
+const int servo4_lower_limit;
+
+int servo5_value;
+const int servo5_upper_limit;
+const int servo5_lower_limit;
+
+void setup() {
+	Serial.begin(9600);
+	
+}
+
+void loop() {
+	
+}
+
+// Functions
+boolean incrementServoValue(int servoValue) {
+
+}
+
+boolean decrementServoValue(int servoValue) {
+
+}
+
+void sendCommand(int servoNumber, int servoValue) {
+	// Readies the command to be sent one character at a time.
+	
+
+}
+
