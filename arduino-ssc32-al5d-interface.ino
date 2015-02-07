f815c851dfb5af1a0bebb0bada38164205d75849
Petri Nissilä <petri.nissila89@gmail.com>
2015-02-07 22:21:35 +0200

    Vaihtoehtoinen yritelmä, ei toimi

--- arduino-ssc32-al5d-interface.ino
+++ arduino-ssc32-al5d-interface.ino
@@ -2,47 +2,53 @@
 
 #include <math.h>
 
-//pohja
-int switchBase = 0;
-int newBaseValue = 1500;
-int baseValue = 1500;
-const double baseHeight = 68;
+//---------------------------------VAKIOT-------------------------------------
+	const double baseHeight = 68;
+	const double shoulderLength = 146;
+	const double elbowLength = 186;
+	const double handLength = 86;
 
-// Olkapää
-int shoulderValue = 0;
-const double shoulderLength = 146;
-double shoulderLimitAngleLower = 0;
+//--------------------------------MUUTTUJAT-----------------------------------
+// kytkimet
+	int switchBase = 0;
+	int switchIncrementWristHeight = 0;
+	int switchDecrementWristHeight = 0;
+	int switchIncrementWristDistance = 0;
+	int switchDecrementWristDistance = 0;
+	int switchIncrementHandAngle = 0;
+	int switchDecrementHandAngle = 0;
+	int switchGrip = 0; 
 
-// Kyynärpää
-int elbowValue = 1500;
-const double elbowLength = 186;
-double elbowLimitAngleLower = 20 * PI / 180;
+// servojen parametrit
+	int baseValue = 1500;
+	int newBaseValue = 1500;
 
-// Ranteen korkeus
-int switchIncrementWristHeight = 0;
-int switchDecrementWristHeight = 0;
-double WristHeight = 200;
-double WristHeightPrime = -(WristHeight - baseHeight);
+	int shoulderValue = 1550;
+	int elbowValue = 1000;
+	int wristValue = 2180;
+	int gripValue = 1160;
+	int newGripValue = 1160;
 
-// Ranteen etäisyys
-int switchIncrementWristDistance = 0;
-int switchDecrementWristDistance = 0;
-double WristDistance = 200;		// Jotain näihin arvoiksi, arvot mm?
+// servojen rajat
+	// double shoulderLimitAngleLower = 0;
+	// double elbowLimitAngleLower = 20 * PI / 180;
 
-// Ranteen asento
-int switchWristDown = 0;
-int switchWristUp = 0;
-int wristValue = 1500;
-
-// Koura
-int switchGrip = 0; 
-int gripValue = 1100;
-int newGripValue = 1100;
-
-
-//Vakio aika 1 sekunti
-int basicTime =2000;
-double transitionSpeed = 10.0;
+// käänteiskinematiikka
+	// paikkaparametrit
+	double CoordX = 100;
+	double CoordY = 100;
+	
+	double wristDistanceX = 100;
+	double wristDistanceY = 100;
+	double handAngle = 0;
+	double wristDistance = sqrt(1 / (pow(wristDistanceX, 2) + pow((wristDistanceY),2)) - 1);
+	
+	// kulmat
+	double theta1 = 1;
+	double theta2 = 1;
+// Muutosparametrit
+	int basicTime =2000;
+	double transitionSpeed = 10.0;
 
 void setup(){
   Serial.begin(9600);
@@ -80,50 +86,49 @@ void loop(){
 	switchDecrementWristDistance = digitalRead(3);
 	switchIncrementWristHeight = digitalRead(4);
 	switchDecrementWristHeight = digitalRead(5);
-	switchWristUp = digitalRead(6);
-	switchWristDown = digitalRead(7);
+	switchIncrementHandAngle = digitalRead(6);
+	switchDecrementHandAngle = digitalRead(7);
 	switchBase = analogRead(A0);
 	switchGrip = analogRead(A1);
      
 	if (switchIncrementWristDistance == HIGH) {
-		WristDistance += transitionSpeed;
+		CoordX += transitionSpeed;
 		updatePositions();
 	}
   if (switchDecrementWristDistance == HIGH) {
-		WristDistance -= transitionSpeed;
+		CoordX -= transitionSpeed;
 		updatePositions();
 	}
   if (switchIncrementWristHeight == HIGH) {
-		WristHeight += transitionSpeed;
+		CoordY += transitionSpeed;
 		updatePositions();
 	}
   if (switchDecrementWristHeight == HIGH) {
-		WristHeight -= transitionSpeed;
+		CoordY -= transitionSpeed;
 		updatePositions();
 	}
-  if(switchWristUp == HIGH){
-		wristValue -= transitionSpeed;
+  if(switchIncrementHandAngle == HIGH){
+		wristValue -= 0.10;
 		updatePositions();
   }
-  if(switchWristDown == HIGH){
-		wristValue += transitionSpeed;
+  if(switchDecrementHandAngle == HIGH){
+		wristValue += 0.10;
 		updatePositions();
   }
   
-  newBaseValue = (analogRead(A0) * 1.955) + 500;
+  newBaseValue = (analogRead(A0) * 1.9) + 500;
   if(baseValue != newBaseValue) {
 	baseValue = newBaseValue;
 	updatePositions();
   }
   
-  newGripValue = (analogRead(A1) * 2.444);
+  newGripValue = (analogRead(A1) * 2.4);
   if(gripValue != newGripValue) {
 	if (newGripValue > 1000) {
 		gripValue = newGripValue;
 		updatePositions();
 	}
   }
-  
 }
 
 void updatePositions(){
@@ -138,41 +143,56 @@ void updatePositions(){
 }
 
 void calculateServoValues() {
-	WristHeightPrime = baseHeight - WristHeight;
-	double Theta2 = calculateTheta2(WristDistance, WristHeightPrime);
-	double Theta1 = calculateTheta1(WristDistance, WristHeightPrime);
+	theta2 = calcTheta2();
+	theta1 = calcTheta1();
 	
-	// Ehtolauseet Thetojen rajoille
-	if ((Theta1 >= shoulderLimitAngleLower) && (Theta2 >= elbowLimitAngleLower)) {
-		elbowValue = 1500 / PI * Theta2 + 1000;
-		shoulderValue = 1700 / PI * Theta1 + 700;
-	} else if ((Theta2 < elbowLimitAngleLower) && (Theta1 < shoulderLimitAngleLower)) {
-		elbowValue = 2500;
-		shoulderValue = 700;
-	} else if ((Theta1 >= shoulderLimitAngleLower) && (Theta2 < elbowLimitAngleLower)) {
-		elbowValue = 2500;
-		shoulderValue = 1700 / PI * Theta1 + 700;
-	} else if ((Theta1 < shoulderLimitAngleLower) && (Theta2 >= elbowLimitAngleLower)) {
-		elbowValue = 1500 / PI * Theta2 + 1000;
-		shoulderValue = 700;
-	} else {
-		elbowValue = 1000;
-		shoulderValue = 1550;
-		String yolo = "";
-		Serial.print(yolo + "Theta1: " + Theta1 + "; Theta2:" + Theta2);
-	}
+	String emptyStr = "";
+	Serial.println(emptyStr + "Shoulder: " + shoulderValue + " Elbow: " + elbowValue + " Theta1: " 
+	+ theta1 + " Theta2: " + theta2 + " WristHeight: " + wristDistanceY + " WristDistance: " + wristDistanceX);
+	
+	elbowValue = 1500 / PI * theta2 + 1000;
+	shoulderValue = 1700 / PI * theta1 + 700;
 }
 
-double calculateTheta2(double X, double Y) {
-	double baseArg((X * X + Y * Y - shoulderLength * shoulderLength - elbowLength * elbowLength)
-					/ 2 * shoulderLength * elbowLength);
-	double result = atan2(sqrt(pow((1 - baseArg), 2)), baseArg);
+double calcWristDistanceX() {
+	return ((CoordX - handLength * cos(handAngle)) / (2 * shoulderLength));
+}
+
+double calcWristDistanceY() {
+	return ((CoordY - baseHeight - handLength * cos(handAngle)) / (2 * shoulderLength));
+}
+
+double calcFingerDistance() {
+	return sqrt(1 / (pow(calcWristDistanceX(), 2) + pow(calcWristDistanceY(), 2)) - 1);
+}
+
+double calcShoulderGroundAngle() {
+	return atan2(calcWristDistanceX() - calcFingerDistance() * calcWristDistanceY(), calcWristDistanceY() + calcFingerDistance() * calcWristDistanceX());
+}
+
+double calcElbowGroundAngle() {
+	return atan2(calcWristDistanceX() + calcFingerDistance() * calcWristDistanceY(), calcWristDistanceY() - calcFingerDistance() * calcWristDistanceX());
+}
+
+double calcTheta1() {
+	double subResult = calcShoulderGroundAngle() - PI;
+	double result = subResult + subResult;
 	return result;
 }
 
-double calculateTheta1(double X, double Y) {
-	double k1 = shoulderLength + elbowLength * cos(calculateTheta2(X, Y));
-	double k2 = elbowLength * sin(calculateTheta2(X, Y));
-	double result = atan2(Y, X) - atan2(k2, k1);
+double calcTheta2() {
+	double subResult = calcElbowGroundAngle() - calcTheta1();
+	double result = subResult + subResult;
 	return result;
 }
+
+double calcTheta3() {
+	double subResult = handAngle - calcElbowGroundAngle();
+	double result = subResult + subResult;
+	return result;
+}
+
+// void printInfo() {
+	// String emptyStr = "";
+	// Serial.println(emptyStr + "Shoulder: " + shoulderValue + " Elbow: " + elbowValue + " Theta1: " + Theta1 + " Theta2: " + Theta2);
+// }
